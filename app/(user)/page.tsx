102b1876a637adf7967fc1f6f661830526896d88
nsect <nsecttheory@gmail.com>
2023-01-05 10:33:00 -0800

    base preview mode setup and test

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,10 +1,13 @@
-
-
+import { previewData } from 'next/headers';
 
 function HomePage() {
+  if (previewData()) {
+    return <div>Preview Mode</div>;
+  }
   return (
-    <div className="text-red-500">
+    <div>
+      <h1>Not in Preview Mode</h1>
     </div>
-  )
+  );
 }
-export default HomePage
+export default HomePage;
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,18 @@
 function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+      <div>
+        <h1 className="text-7xl">Brendan's Weekly Blog</h1>
+        <h2 className="mt-5 md:mt-0">
+          Welcome to{' '}
+          <span className="underline decoration-4 decoration-[#40e0d0]">Every small business owners</span>{' '}
+          favourite blog in the Devosphere.
+        </h2>
+      </div>
+      <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+        New service offers | Latest in Technology | Weekly tools and more!
+      </p>
+    </div>
+  );
 }
 export default Banner
