725be6534752e4c41557ed5b20d8ebe90305d586
oroiba <oroiba@proton.me>
2023-06-04 16:36:22 -0700

    final adjustments/relaunch

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,12 +5,12 @@ function Banner() {
         <h1 className="text-7xl">Brendan's Weekly Blog</h1>
         <h2 className="mt-5 md:mt-0">
           Welcome to{' '}
-          <span className="underline decoration-4 decoration-[#40e0d0]">Every small business owners</span>{' '}
-          favourite blog in the Devosphere.
+          <span className="underline decoration-4 decoration-[#40e0d0]">a place to learn about</span>{' '}
+          my development roadmap.
         </h2>
       </div>
       <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
-        New service offers | Latest in Technology | Weekly tools and more!
+        New service offers | Latest in my Portfolio | tools, articles and more!
       </p>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ function Header() {
     <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
       <div className="flex items-center space-x-2">
         <Link href="/">
-          <Image src={logoWhite} width={150} height={150}  alt="logo" />
+          <Image src={logoWhite} width={150} height={150} alt="logo" />
         </Link>
       </div>
       <div className="hidden sm:flex">
         <Link
           className="px-5 py-3 text-sm md:text-base bg-gray-900 text-[#40e0d0] flex items-center rounded-full text-center"
-          href="https://brendanshields.vercel.app/#contact"
+          href="https://brendan-dev.vercel.app/"
         >
           Contact me to get your next project started
         </Link>
